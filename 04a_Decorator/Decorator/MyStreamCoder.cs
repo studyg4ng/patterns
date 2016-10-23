a51a56a32b02a9a4f08fad774ee2475d7098dfb3
Thomas Peter Siller <tsiller.mmt-b2015@fh-salzburg.ac.at>
2016-10-23 10:06:01 +0200

    finished 04a_decorator

---
+++ 04a_Decorator/Decorator/MyStreamCoder.cs
@@ -0,0 +1,95 @@
+﻿/*1510601027 fhs38532
+  Thomas Siller*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Decorator {
+    abstract class StreamEncoderDecoder : Stream {
+        Stream _stream;
+        Boolean _decode;
+
+        public StreamEncoderDecoder(Stream stream, Boolean decode) {
+            this._stream = stream;
+            this._decode = decode;
+        }
+
+        public override bool CanRead
+        {
+            get
+            {
+                return this._stream.CanRead;
+            }
+        }
+
+        public override bool CanSeek
+        {
+            get
+            {
+                return this._stream.CanSeek;
+            }
+        }
+
+        public override bool CanWrite
+        {
+            get
+            {
+                return this._stream.CanWrite;
+            }
+        }
+
+        public override long Length
+        {
+            get
+            {
+                return this._stream.Length;
+            }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                return this._stream.Position;
+            }
+
+            set
+            {
+                this._stream.Position = value;
+            }
+        }
+
+        public override void Flush() {
+            this._stream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count) {
+            int result = this._stream.Read(buffer, offset, count);
+            for(int i = 0; i < buffer.Length; i++) {
+                if(this._decode) buffer[i] = Decode(buffer[i]);
+                else buffer[i] = Encode(buffer[i]);
+            }
+            return result;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin) {
+            return this._stream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value) {
+            this._stream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count) {
+            this._stream.Write(buffer, offset, count);
+        }
+
+        public abstract byte Encode(byte b);
+
+        public abstract byte Decode(byte b);
+    }
+}
