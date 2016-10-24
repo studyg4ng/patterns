f535b8c8e04a48fdb94e031bb684c1da1ae1397c
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2016-10-24 13:49:36 +0200

    start hw05

---
+++ 05a_Composite/05a_Composite/Program.cs
@@ -0,0 +1,42 @@
+﻿/*1510601027 fhs38532
+  Thomas Siller
+  -------------------
+  1510601032 fhs38596
+  Patrick Obermüller*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05a_Composite {
+    class Program {
+        static void Main(string[] args) {
+
+            Task monday = new Task(0, "MONTAG");
+
+            Task homework = new Task(0, "HOMEWORK");
+            Task morning = new Task(0, "MORNING");
+            Task getOutOfBed = new Task(0, "EAT");
+
+            getOutOfBed.add(new Task(20, "Eat Sausages"));
+            getOutOfBed.add(new Task(20, "Eat Bacon"));
+            getOutOfBed.add(new Task(20, "Eat Pumpkin"));
+
+            morning.add(new Task(20, "Put on Clothes"));
+            morning.add(new Task(20, "Brush teeth"));
+            morning.add(new Task(20, "Prepare food"));
+
+            homework.add(new Task(1, "Open Book"));
+            homework.add(new Task(333, "Do ........ homework"));
+            homework.add(new Task(333, "Push to Master"));
+
+            morning.add(getOutOfBed);
+            monday.add(morning);
+            monday.add(homework);
+
+            monday.printTaskDescription("");
+        }
+    }
+}
---
+++ 05a_Composite/05a_Composite/Task.cs
@@ -0,0 +1,53 @@
+﻿/*1510601027 fhs38532
+  Thomas Siller
+  -------------------
+  1510601032 fhs38596
+  Patrick Obermüller*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05a_Composite {
+    class Task {
+        private List<Task> _tasks = new List<Task>();
+        private double _time;
+        private string _description;
+
+        public Task(double time = 0, string description = "!description missing!") {
+            this._time = time;
+            this._description = description;
+        }
+
+        public void add(Task task) {
+            this._tasks.Add(task);
+        }
+
+        public void remove(Task task) {
+            this._tasks.Remove(task);
+        }
+
+        public double getTime() {
+            double timeSum = this._time;
+
+            foreach (Task t in _tasks) {
+                timeSum += t.getTime();
+            }
+            return timeSum;
+        }
+
+        public string formateTaskInformation() {
+            return String.Format("{0} ({1}t)", this._description, this.getTime());
+        }
+
+        public void printTaskDescription(string tmp) {
+            Console.WriteLine(tmp + this.formateTaskInformation());
+
+            foreach (Task t in _tasks) {
+                t.printTaskDescription("-" + tmp);
+            }
+        }
+    }
+}
